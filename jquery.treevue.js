ca5082693ab405dceae9d4553760ac90f33804e2
Wilco Fiers <wilcofiers@gmail.com>
2013-06-05 11:27:39 +0200

    selected state updates in sync with checkboxes

--- jquery.treevue.js
+++ jquery.treevue.js
@@ -12,23 +12,23 @@
     /**
      * Add ARIA roles
      */
-    function addAriaTreeRoles($trees) {
+    function addAriaTreeRoles(trees) {
         var collapsed;
         
         // Insert the tree in a div with role=application
-        $trees.wrap("<div role='application'></div>");
+        trees.wrap("<div role='application'></div>");
         
-        $trees.find('li').attr( // define tree nodes
+        trees.find('li').attr( // define tree nodes
             'role', 'treeitem'
         )
-        $trees.find('ul').attr({ // define branches
+        trees.find('ul').attr({ // define branches
             'role': 'group'
         }).closest('li').attr('aria-expanded', 'true');
         
-        $trees.attr('role', 'tree');
+        trees.attr('role', 'tree');
         
         // Collapse nodes nested within a ul with aria-hidden
-        collapsed = $trees.find('ul[aria-hidden=true]').closest('li');
+        collapsed = trees.find('ul[aria-hidden=true]').closest('li');
         collapsed = collapsed.find('[aria-expanded=true]').andSelf();
         collapsed.attr('aria-expanded', 'false');
         collapsed.find('ul').attr('aria-hidden', 'true');
@@ -38,8 +38,8 @@
     /**
      * Where checkboxes are included allow selection
      */
-    function addAriaSelectStates($trees) {
-        var $selectable = $trees.find(':checkbox').attr({
+    function addAriaSelectStates(trees) {
+        var selectable = trees.find(':checkbox').attr({
             'tabindex': '-1'
         }).closest('li').attr({
             'aria-selected': 'false'
@@ -49,28 +49,24 @@
     /**
      * Move the focus to an item in the tree.
      */
-    function focusItem($elm) {
-        var $tree = $elm.closest('.treevue');
-        $tree.find('.' + focusClass).removeClass(focusClass).attr('tabindex', -1);
-        $elm.focus().attr('tabindex', 0);
-        $elm.addClass(focusClass);
-        
-        if ($elm.context.tagName !== 'LI') {
-            alert('fout');
-            console.log($elm);
-        }
+    function focusItem(elm) {
+        var tree = elm.closest('.treevue');
+        tree.find('.' + focusClass).removeClass(focusClass).attr('tabindex', -1);
+        elm.focus().attr('tabindex', 0);
+        elm.addClass(focusClass);
     }
     
     /**
      * Toggle the visibility of the branch
      */
     function toggleBranch(branch) {
+        var subtree = branch.children().filter('ul');
         if (branch.attr('aria-expanded') === 'true') {
             branch.attr('aria-expanded', 'false');
-            branch.children().filter('ul').hide(200).attr('aria-hidden', 'true');
+            subtree.hide(200).attr('aria-hidden', 'true');
         } else {
             branch.attr('aria-expanded', 'true');
-            branch.children().filter('ul').show(200).attr('aria-hidden', 'false');
+            subtree.show(200).attr('aria-hidden', 'false');
         }
     }
     
@@ -78,15 +74,24 @@
      * TreeVue jQuery plugin; accessible treeview
      */
     $.fn.treevue = function() {
-        var $trees = $(this);
-        $trees.addClass('treevue');
-        $trees.find('li').attr('tabindex', -1);
-        $trees.find('> :first-child').attr('tabindex', 0).addClass(focusClass);
+        var trees = $(this);
+        trees.addClass('treevue');
+        trees.find('li').attr('tabindex', -1);
+        trees.find('> :first-child').attr('tabindex', 0).addClass(focusClass);
         
-        addAriaTreeRoles($trees);
+        addAriaTreeRoles(trees);
         
-        addAriaSelectStates($trees)
+        addAriaSelectStates(trees)
     };
+    
+    /**
+     * When a checkbox is changed, update the aria-selected state.
+     */
+    function checkboxChange(box) {
+        box.closest('[aria-selected]').attr(
+                    'aria-selected', (box.prop('checked') ? 'true' : 'false'));
+    }
+    
     
     // When the document is loaded, attach event handlers for all vuetrees 
     $(function () {
@@ -100,31 +105,27 @@
         
         // Keep aria-selected state in sync with the checkbox
         }).on('change', '.treevue :checkbox', function (event) {
-            console.log(123);
-            $(this).closest('[aria-selected]').attr(
-                    'aria-selected', (this.checked ? 'true' : 'false'));
+            checkboxChange($(this));
         
         // keyboard input
         }).on('keydown', '.treevue li', function (event) {
-            var expanded, selected,
+            var expanded, 
+                keyCode = event.keyCode,
                 $this = $(this);
             
             if (event.target !== this) {
                 return;
             }
             expanded = $this.attr('aria-expanded');
-            selected = $this.attr('aria-selected');
             
             // Press RETURN
-            if (event.keyCode === 13 && selected !== undefined) {
+            if (keyCode === 13 && $this.attr('aria-selected') !== undefined) {
+                //locate the checkbox and invert it and the select value
                 var checkbox = $this.find(':checkbox').first();
-                if (checkbox.is(':checked')) {
-                    checkbox.attr('checked', false);
-                } else {
-                    checkbox.attr('checked', true);
-                }
+                checkbox.prop('checked', !checkbox.prop('checked'));
+                checkboxChange(checkbox);
                 
-            } else if (event.keyCode === 40) { // press DOWN
+            } else if (keyCode === 40) { // press DOWN
                 if (expanded === 'true') { // enter a branch
                     focusItem($this.find('ul li').first());
                 } else if ($this.next().length === 0) { // exit a branch
@@ -133,7 +134,7 @@
                     focusItem($this.next());
                 }
                 
-            } else if (event.keyCode === 38) { // press UP
+            } else if (keyCode === 38) { // press UP
                 if ($this.prev().attr('aria-expanded') === 'true') { // enter a branch
                     focusItem($this.prev().find('ul li').last());
                 } else if ($this.prev().length === 0) { // exit a branch
@@ -142,14 +143,14 @@
                     focusItem($this.prev());
                 }
                 
-            } else if (event.keyCode === 37) { // press LEFT
+            } else if (keyCode === 37) { // press LEFT
                 if (expanded === 'true') {
                     toggleBranch($this);
                 } else { // exit the current branch
                     focusItem($this.parent().parent());
                 }
                 
-            } else if (event.keyCode === 39) { // press RIGHT
+            } else if (keyCode === 39) { // press RIGHT
                 if (expanded === 'false') {
                     toggleBranch($this);
                 } else if (expanded === 'true') { // enter a branch
