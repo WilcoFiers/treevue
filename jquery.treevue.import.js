34ed730383ecbab21555a56c81a72ea51ab09a77
Wilco Fiers <wilcofiers@gmail.com>
2013-06-11 14:02:33 +0200

    Set up an example for testing the import

---
+++ jquery.treevue.import.js
@@ -0,0 +1,13 @@
+(function ($) {
+    'use strict';
+    
+    /**
+     * Create a treevue based on a json object
+     */
+    $.treevue = function (json) {
+        var tree = $('<ul class="treevue"/>');
+        
+        return tree;
+    };
+    
+}(jQuery));
